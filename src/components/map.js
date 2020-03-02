2421a95cedea05b22d6cdadd87637a2e6f3678f0
fishmanjohn <john@emmaandjohnbyvan.com>
2020-03-02 12:06:45 -0800

    added changes to map functonality

--- src/components/map.js
+++ src/components/map.js
@@ -1,5 +1,5 @@
 import React,{useState, useEffect} from 'react';
-import ReactMapGL, {Marker} from 'react-map-gl'
+import ReactMapGL, {Marker, Popup} from 'react-map-gl'
 import axios from 'axios'
 
 function Map(){
@@ -10,7 +10,10 @@ const [viewport, setViewport] = useState({
     height: "900px",
     zoom: 10
 });
+
 const [gems,setGems] = useState([])
+
+const [selectedGem, setSelectedGem] = useState(null)
 
 useEffect(()=>{
     axios.get('https://geoseek-be-stage.herokuapp.com/api/gems')
@@ -34,9 +37,23 @@ useEffect(()=>{
         >
             {gems.map((gem)=>(
                 <Marker key = {gem.id} latitude = {gem.latitude} longitude = {gem.longitude}>
-                    <div>GEM</div>
+                    <button className = 'marker-btn' onClick = {e =>{
+                        e.preventDefault()
+                        setSelectedGem(gem)
+                    }} >
+                        <img src = '/gem.png' alt = "Gem Icon"/>
+                    </button>
                 </Marker>
             ))}
+            {selectedGem && (
+                <Popup latitude = {selectedGem.latitude} longitude = {selectedGem.longitude} onClose = {()=>setSelectedGem(null)}>
+                    <div>
+                        <h2>{`Title: ${selectedGem.title}`}</h2>
+                        <p>{`Dificulty: ${selectedGem.difficulty}`}</p>
+                        <p>{`Description: ${selectedGem.description}`}</p>
+                    </div>
+                </Popup>
+            )}
         </ReactMapGL>
 
         
