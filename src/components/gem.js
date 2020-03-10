c9c43cc8e73d72e8e40cd5e7fb87eb654dd3d447
dantebui <senpaicaboose@gmail.com>
2020-03-10 08:54:03 -0700

    basic search functionality

--- src/components/gem.js
+++ src/components/gem.js
@@ -1,32 +1,20 @@
 import React from 'react'
-<<<<<<< HEAD
-
-=======
 import styled from 'styled-components'
 
 const Gem = styled.div`
 color:white;
 `
->>>>>>> 318ee86fb8aebbd35cbdcc64779916b8b1273676
 
 function GemCard ({title, latitude, longitude}) {
 
 
     return (
-<<<<<<< HEAD
-        <div className='gem-card'>
-=======
         <Gem>
->>>>>>> 318ee86fb8aebbd35cbdcc64779916b8b1273676
             <h2>{title}</h2>
             <p>Lat: {latitude}</p>
             <p>Lon: {longitude}</p>
 
-<<<<<<< HEAD
-        </div>
-=======
         </Gem>
->>>>>>> 318ee86fb8aebbd35cbdcc64779916b8b1273676
     );
 }
 
--- src/components/viewGem.js
+++ src/components/viewGem.js
@@ -4,47 +4,65 @@ import axios from 'axios'
 import GemCard from './gem'
 import styled from 'styled-components'
 
+const Card = styled.div`
+margin: 20px;
+padding: 5px;
+border-radius: 10px;
+display: flex;
+width: 200px;
+justify-content: space-between;
+
+`
+
+const Card2 = styled.div`
+border: 2px solid teal;
+display: flex;
+justify-content: space-evenly;
+flex-wrap: wrap;
+width: 350px;
+padding: 0px;
+max-height: 100%;
+min-height: 100vh;
+height: 100vh;
+overflow-y: auto;
+`
+
 export default function ViewGem ({updatePosition}) {
     const [gems, setGems] = useState([])
+    const [search, setSearch]= useState('')
+    const [filtered, setFiltered]= useState([])
+
     useEffect(() => {
         axios.get('https://geoseek-be-stage.herokuapp.com/api/gems')
             .then(res => {
-                console.log(res)
                 setGems(res.data)
             })
             .catch(err => {
                 console.log(err)
             })
     }, [])
-
-    const Card = styled.div`
-    margin: 20px;
-    padding: 5px;
-    border-radius: 10px;
-    display: flex;
-    width: 200px;
-    justify-content: space-between;
-    
-    `
-
-    const Card2 = styled.div`
-    border: 2px solid teal;
-    display: flex;
-    justify-content: space-evenly;
-    flex-wrap: wrap;
-    width: 350px;
-    padding: 0px;
-    max-height: 100%;
-    min-height: 100vh;
-    height: 100vh;
-    overflow-y: auto;
-    `
-    
-
     return (
         <Card2>
-            {gems.map((gem) => {
-
+            <form>
+                <input
+                    name= 'searchForm'
+                    placeholder= 'Search Gems'
+                    value= {search}
+                    onChange= {e=>{
+                        setSearch(e.target.value.toLowerCase())
+                        console.log(search)
+                        const filteredGems= gems.filter(gem=>{
+                            const ls= gem.title.toLowerCase()
+                            // console.log(ls, 'ls')
+                            // setFiltered(ls.search(search))
+                            // console.log(filtered, 'filtered')
+                            return ls.search(search) !== -1
+                        })
+                        setFiltered(filteredGems)
+                    }}
+                />
+            </form>
+            {filtered.map((gem) => {
                 return (
                     <div>
                         <Card>
