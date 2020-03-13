594508901565c5d47bda0f8031774a2e4bfe98b0
Anthony Lopez <anthonybeconscious@gmail.com>
2020-03-13 14:42:15 -0600

    fixed bug where blank address field will generate coordinates. added alert box

--- src/components/createGemRedux.js
+++ src/components/createGemRedux.js
@@ -130,6 +130,7 @@ function CreateGem (props) {
 
     const handleGeocodeSubmit = e => {
         e.preventDefault()
+        if(address === ''){alert('enter an address') && setAddress('')}else{
         function geocode (address) {
             axios
                 .get(`https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates?f=json&singleLine=${ address.address }&outFields=Match_addr,Addr_type`)
@@ -149,6 +150,7 @@ function CreateGem (props) {
         }
         geocode(address)
     }
+}
 
     const handleSubmit = e => {
         e.preventDefault()
@@ -246,126 +248,3 @@ export default connect(mapStateToProps,
 )(CreateGem)
 
 
-
-
-
-
-
-
-
-
-
-/////////////////// MY REDUX CODE COPY ///////////////////////
-
-
-// function CreateGem (props) {
-
-
-
-//     const [newGem, setNewGem ] = useState({
-//             title: '',
-//             longitude: '',
-//             latitude: '',
-//             difficulty: '',
-//             description: ''
-//     })
-
-//     const handleChanges = e =>{
-//         setNewGem({
-//             ...newGem,
-//             [e.target.name]: e.target.value
-//         })
-//     }
-//     const handleSubmit = e =>{
-//         e.preventDefault()
-//         // props.postGem(newGem)
-//         setNewGem({
-//             title: '',
-//             longitude: '',
-//             latitude: '',
-//             difficulty: '',
-//             description: ''
-//         })
-//     }
-
-//     return(
-//         <form onSubmit={handleSubmit}>
-//             <input
-//             name='title'
-//             placeholder='Title'
-//             value={newGem.name}
-//             onChange={handleChanges}
-//             />
-
-//             <GeocodingRedux/>
-
-//             <input
-//             name='difficulty'
-//             placeholder='Dificulty. 1-5'
-//             value={newGem.difficulty}
-//             onChange={handleChanges}
-//             />
-//             <input
-//             name='description'
-//             placeholder='Description.'
-//             value={newGem.description}
-//             onChange={handleChanges}
-//             />
-//             <button type='submit'>Add Gem!</button>
-//         </form>
-//     )
-// }
-// const mapStateToProps = state => {
-//     return{
-//         state
-//     }
-
-// }
-
-// export default connect(mapStateToProps, 
-//     { postGem }
-//     )(CreateGem)
-
-
-
-
-
-///////////copy of form before changes for address
-//     <form onSubmit={handleSubmit}>
-//     <input
-//     name='title'
-//     placeholder='Title'
-//     value={newGem.name}
-//     onChange={(e)=>{
-//         setNewGem({
-//             ...newGem,
-//     [e.target.name]:e.target.value
-// })
-//     }}
-//     />
-//     <input
-//     name='longitude'
-//     placeholder='Logitude.'
-//     value={newGem.longitude}
-//     onChange={handleChanges}
-//     />
-//     <input
-//     name='latitude'
-//     placeholder='Latitude.'
-//     value={newGem.latitude}
-//     onChange={handleChanges}
-//     />
-//     <input
-//     name='difficulty'
-//     placeholder='Dificulty. 1-5'
-//     value={newGem.difficulty}
-//     onChange={handleChanges}
-//     />
-//     <input
-//     name='description'
-//     placeholder='Description.'
-//     value={newGem.description}
-//     onChange={handleChanges}
-//     />
-//     <button type='submit'>Add Gem!</button>
-// </form>
